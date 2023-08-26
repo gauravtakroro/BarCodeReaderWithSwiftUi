7822588c2599464332735d1bbef86db82d995616
Gaurav Tak <gaurav.tak@roro.io>
2023-08-26 15:14:43 +0530

    Utils Methods added for checking url and other  uses

--- BarCodeReaderWithSwiftUi/HomeUi/HomeView.swift
+++ BarCodeReaderWithSwiftUi/HomeUi/HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State var scannedBarCodeValue: String = ""
+    
     var body: some View {
         VStack {
             Spacer()
@@ -16,6 +19,29 @@ struct HomeView: View {
                 .foregroundColor(.black)
             Text("BarCode Reader Demo").padding(.top, 24)
             Text("please tap bottom button for BarCode scanning").padding(.top, 8).font(.system(size: 14)).foregroundColor(Color.black)
+            
+            if scannedBarCodeValue.isEmpty {
+                Text("No Bar Code is scanned yet.").font(.system(size: 20)).foregroundColor(Color.gray.opacity(0.8))
+                    .padding(.top, 24)
+            } else {
+                VStack {
+                    Text("Most Recent Scanned Bar Code Value").foregroundColor(Color.black).padding(.top, 24)
+                    if UtilMethods.verifyUrl(urlString: scannedBarCodeValue) {
+                        Button {
+                            UtilMethods.launchLinkWithSafari(urlString: scannedBarCodeValue)
+                        } label: {
+                            Text(scannedBarCodeValue).foregroundColor(Color.blue).underline().multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true).font(.system(size: 16))
+                        }
+                    } else {
+                        Button {
+                            UIPasteboard.general.string = scannedBarCodeValue
+                            
+                        } label: {
+                            Text(scannedBarCodeValue).foregroundColor(Color.black).multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true).font(.system(size: 16))
+                        }
+                    }
+                }.padding(.top, 36)
+            }
             Spacer()
             Button {
                // tap here to scan bar code
